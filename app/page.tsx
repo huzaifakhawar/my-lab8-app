d0419f88e63c3657442c26badce409f982d7a8da
Huzaifa Khawar <146337569+huzaifakhawar@users.noreply.github.com>
2026-05-23 08:04:27 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 export default function Home() {
-  const appName = process.env.NEXT_PUBLIC_APP_NAME;
-  const studentName = process.env.NEXT_PUBLIC_STUDENT_NAME;
-  const studentRoll = process.env.NEXT_PUBLIC_STUDENT_ROLL;
-
   return (
     <main style={{ textAlign: "center", marginTop: "10%" }}>
       <h1>Lab 8 — Vercel Deployment</h1>
-      <p>App: {appName}</p>
-      <p>Name: {studentName}</p>
-      <p>Roll: {studentRoll}</p>
+      <p>Name: Huzaifa Khawar</p>
+      <p>Roll: 01-131232-034</p>
     </main>
   );
 }
