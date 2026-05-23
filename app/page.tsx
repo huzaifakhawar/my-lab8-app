8e4ebd7793f70cf8b333984c3c086db097e12899
Huzaifa Khawar <146337569+huzaifakhawar@users.noreply.github.com>
2026-05-23 07:38:12 +0500

    Remove duplicate page.tsx file

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", marginTop: "10%" }}>
-      <h1>Lab 8 — Vercel Deployment</h1>
-      <p>Name: [Huzaifa Khawar]</p>
-      <p>Roll: [01-131232-034]</p>
-    </main>
-  );
-}
